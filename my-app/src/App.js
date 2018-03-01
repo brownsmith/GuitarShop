0264d0be1b788c4410c73759729d610dc2d7d64d
Paul Brownsmith <paul.brownsmith@waitrose.co.uk>
2018-03-01 17:50:46 +0000

    Added container for ProductsWrapper and default route

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
 import './App.css';
-import ProductWrapper from './components/ProductsWrapper/ProductsWrapper';
-import products from './api/products.json';
+import ProductWrapper from './containers/ProductsWrapper/ProductsWrapper';
 
 export default class App extends Component {
-
-  _loadData = () => {
-    return products;
-  };
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">The Guitar Store</h1>
         </header>
-        <ProductWrapper data={this._loadData()} />
+        <main>
+          <Route exact path="/" component={ProductWrapper} />
+        </main>
       </div>
-    );
+    )
   }
+  
 };
