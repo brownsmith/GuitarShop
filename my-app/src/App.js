fcf09ba1e74e7278c94de0028da10e7fbd8f4e65
Paul Brownsmith <paul.brownsmith@waitrose.co.uk>
2018-03-02 14:02:51 +0000

    BrowserRouter and Provider added to App.js, test green

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
+import { Route, BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 import './App.css';
 import ProductWrapper from './containers/ProductsWrapper/ProductsWrapper';
 
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">The Guitar Store</h1>
-        </header>
-        <main>
-          <Route exact path="/" component={ProductWrapper} />
-        </main>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">The Guitar Store</h1>
+          </header>
+          <main>
+            <BrowserRouter>
+              <Route exact path="/" component={ProductWrapper} />
+            </BrowserRouter>
+          </main>
+        </div>
+      </Provider>
     )
-  }
-  
+  }  
 };
+
+// Notes on <Provider store>:
+// https://github.com/reactjs/react-redux/blob/master/docs/api.md
