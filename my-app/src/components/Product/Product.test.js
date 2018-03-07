4bcc845937eee4993274245ac4228e009d8e2a55
Paul Brownsmith <paul.brownsmith@waitrose.co.uk>
2018-03-07 21:21:57 +0000

    Product test file

---
+++ my-app/src/components/Product/Product.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Product from './Product';
+
+configure({ adapter: new Adapter() });
+
+describe('Product component', () => {
+  it('renders a product component', () => {
+    const productDetails = {
+      spec: {
+        image: 'black-strat.jpg',
+      },
+    };
+    const div = document.createElement('div');
+    ReactDOM.render(<Product productDetails={productDetails} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
