afa25449e879fdda1f62e351c1f68154b92f2e2c
Paul Brownsmith <paul.brownsmith@waitrose.co.uk>
2018-03-14 20:26:33 +0000

    Removing Provider and ConnectedRouter components from index

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'react-router-redux';
-import store, { history } from './store'; // store is default export, history is named export
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 const target = document.querySelector('#root');
 
-render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <App />
-    </ConnectedRouter>
-  </Provider>,
-  target
-);
+render(<App />, target);
 
 registerServiceWorker();
