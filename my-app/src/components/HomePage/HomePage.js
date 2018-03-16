2e29afeecff9c95d0ad394c61c4db1494e04d3e7
Paul Brownsmith <paul.brownsmith@waitrose.co.uk>
2018-03-16 15:36:05 +0000

    Homepage component

---
+++ my-app/src/components/HomePage/HomePage.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <h4>welcome to the shop</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in
+          enim consectetur, luctus arcu et, euismod nulla. Sed facilisis
+          convallis porta. Proin sodales lectus vel commodo vestibulum. Aliquam
+          sit amet rhoncus elit. Curabitur maximus mauris ac ex gravida euismod.
+          Aliquam ullamcorper consequat ipsum in elementum. Duis sed egestas
+          libero. Morbi fringilla lobortis nunc, malesuada laoreet odio gravida
+          non. Suspendisse at magna sit amet velit vulputate finibus eget nec
+          lectus. Aliquam varius sodales ex, at euismod dolor scelerisque non.
+          Donec mauris lacus, rhoncus eget odio iaculis, congue elementum metus.
+          Integer mi lacus, sagittis in lorem quis, suscipit pharetra erat.
+          Phasellus sagittis porttitor vehicula.
+        </p>
+      </div>
+    );
+  }
+}
