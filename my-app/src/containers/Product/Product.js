8501e3e15965d4dff2f2613ada6b556bee72a992
Paul Brownsmith <paul.brownsmith@waitrose.co.uk>
2018-03-20 19:33:12 +0000

    function to return product brand, product selector js created

--- my-app/src/containers/Product/Product.js
+++ my-app/src/containers/Product/Product.js
@@ -3,6 +3,7 @@ import Product from '../../components/Product/Product.js';
 import { withRouter } from 'react-router-dom';
 import { ADD_TO_CART } from '../../modules/actions/trolley.js';
 import { addProductToCart } from '../../modules/actions/trolley.js';
+import { getProductBrand } from '../../modules/selectors/product.js';
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -13,7 +14,7 @@ const mapDispatchToProps = dispatch => {
 
 const mapStateToProps = state => {
   return {
-    title: 'Fender',
+    title: getProductBrand(state),
   };
 };
 
