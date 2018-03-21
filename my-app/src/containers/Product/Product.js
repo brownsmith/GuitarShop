0e06219b34d4ca0d6c335214ab168d3cbeb269b0
Paul Brownsmith <paul.brownsmith@waitrose.co.uk>
2018-03-21 04:02:08 +0000

    note in Product.js

--- my-app/src/containers/Product/Product.js
+++ my-app/src/containers/Product/Product.js
@@ -4,6 +4,11 @@ import { withRouter } from 'react-router-dom';
 import { ADD_TO_CART } from '../../modules/actions/trolley.js';
 import { addProductToCart } from '../../modules/actions/trolley.js';
 import { getProductBrand } from '../../modules/selectors/product.js';
+/* 
+  getProductBrand is a useless function, but this file is my only 
+  example of a container that has both mapDispatchToProps and 
+  mapStateToProps to play with
+*/
 
 const mapDispatchToProps = dispatch => {
   return {
