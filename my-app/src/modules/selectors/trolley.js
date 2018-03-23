a4f8d866b85b9f0e5758c95c41b1951b3b466560
Paul Brownsmith <paul.brownsmith@waitrose.co.uk>
2018-03-23 16:14:10 +0000

    getTrolleyTotalPrice refactor closes #13

--- my-app/src/modules/selectors/trolley.js
+++ my-app/src/modules/selectors/trolley.js
@@ -1,8 +1,8 @@
 export const getTrolleyTotalPrice = state => {
-  let sum = 0;
-  let total = 0;
-  state.trolley.forEach(item => {
-    total = sum += parseInt(item.price.substr(1), 10);
-  });
-  return '£' + total;
+  return (
+    '£' +
+    state.trolley.reduce(function(acc, obj) {
+      return acc + parseInt(obj.price.substr(1), 10);
+    }, 0)
+  );
 };
