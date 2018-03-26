2a7032d2505441a4ec9da08d28ab175521e33ebd
Paul Brownsmith <paul.brownsmith@waitrose.co.uk>
2018-03-26 21:22:15 +0100

    fetch products action, initial file commit

---
+++ my-app/src/modules/actions/products.js
@@ -0,0 +1,8 @@
+export const FETCH_PRODUCTS = 'guitarShop/FETCH_PRODUCTS';
+
+export function fetchProducts(productCatalog) {
+  return {
+    type: FETCH_PRODUCTS,
+    details: productDetails,
+  };
+}
