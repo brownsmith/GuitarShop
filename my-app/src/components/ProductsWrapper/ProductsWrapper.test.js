d252de246f9fdc6984347f7013367f6279929697
Paul Brownsmith <paul.brownsmith@waitrose.co.uk>
2018-03-30 20:46:04 +0100

    fixing ProductsWrapper test

--- my-app/src/components/ProductsWrapper/ProductsWrapper.test.js
+++ my-app/src/components/ProductsWrapper/ProductsWrapper.test.js
@@ -8,9 +8,11 @@ import products from '../../api/products.json';
 configure({ adapter: new Adapter() });
 
 it('should render the ProductsWrapper component', () => {
-  // const data = [];
-  // const productsWrapper = shallow(<ProductsWrapper data={data} loading={false} />);
-  // expect(productsWrapper.find('.productsWrapperComponent').exists()).toEqual(
-  //   true
-  // );
+  const data = [];
+  const productsWrapper = shallow(
+    <ProductsWrapper data={data} loading={false} fetchProducts={() => {}} />
+  );
+  expect(productsWrapper.find('.productsWrapperComponent').exists()).toEqual(
+    true
+  );
 });
