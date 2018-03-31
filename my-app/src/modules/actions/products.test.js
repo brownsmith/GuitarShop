d484445894e56878910257718d6ab5715e6a9eeb
Paul Brownsmith <paul.brownsmith@waitrose.co.uk>
2018-03-31 11:36:18 +0100

    receiveProducts action test

--- my-app/src/modules/actions/products.test.js
+++ my-app/src/modules/actions/products.test.js
@@ -1,5 +1,5 @@
-import { requestProducts } from './products.js';
-import { REQUEST_PRODUCTS } from '../actions/products.js';
+import { requestProducts, receiveProducts } from './products.js';
+import { REQUEST_PRODUCTS, RECEIVE_PRODUCTS } from '../actions/products.js';
 
 describe('products actions', () => {
   it('should dispatch requestProducts', () => {
@@ -8,5 +8,14 @@ describe('products actions', () => {
       loading: true,
     };
     expect(requestProducts({})).toEqual(expectedActions);
+  });
+
+  it('should dispatch receiveProducts', () => {
+    const expectedActions = {
+      type: RECEIVE_PRODUCTS,
+      products: {},
+      loading: false,
+    };
+    expect(receiveProducts({})).toEqual(expectedActions);
   });
 });
