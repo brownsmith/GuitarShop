f41cf301efde19ee47241ff1b4719bcb48657dd7
Paul Brownsmith <paul.brownsmith@waitrose.co.uk>
2018-04-23 08:32:10 +0100

    Trolley page initial set up

---
+++ my-app/src/components/TrolleyPage/TrolleyPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class TrolleyPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>trolley page</h1>
+      </div>
+    );
+  }
+}
