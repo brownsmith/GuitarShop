4ad204590663d3cb31a1e6f875695fb69165d458
Paul Brownsmith <paul.x.brownsmith@waitrose.co.uk>
2020-03-21 14:34:02 +0000

    removing imports no longer used

--- my-app/src/components/ProductsWrapper/ProductsWrapper.test.js
+++ my-app/src/components/ProductsWrapper/ProductsWrapper.test.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import ProductsWrapper from './ProductsWrapper';
-import Product from '../Product/Product';
-import products from '../../api/products.json';
 
 configure({ adapter: new Adapter() });
 
