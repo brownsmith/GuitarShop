5b979534f1a4b80be3a2ae1d19239a5fe3be82ed
Paul X. Brownsmith <paul.x.brownsmith@waitrose.co.uk>
2020-03-27 08:11:51 +0000

    mock useEffect hook in test

--- my-app/src/components/ProductsWrapper/ProductsWrapper.test.js
+++ my-app/src/components/ProductsWrapper/ProductsWrapper.test.js
@@ -5,12 +5,26 @@ import ProductsWrapper from './ProductsWrapper';
 
 configure({ adapter: new Adapter() });
 
-it('should render the ProductsWrapper component', () => {
-  const data = [];
-  const productsWrapper = shallow(
-    <ProductsWrapper data={data} loading={false} fetchProducts={() => {}} />
-  );
-  expect(productsWrapper.find('.productsWrapperComponent').exists()).toEqual(
-    true
-  );
+describe('Product details page', () => {
+  let productsWrapper;
+  it('should render the ProductsWrapper component', () => {
+    const data = [];
+    jest.spyOn(React, 'useEffect').mockImplementation(f => f());
+    productsWrapper = shallow(
+      <ProductsWrapper data={data} loading={false} fetchProducts={() => {}} />
+    );
+    expect(productsWrapper.find('.productsWrapperComponent').exists()).toEqual(
+      true
+    );
+  });
+
+  // it('should dp something else witht productData from the hook', () => {
+  //   const data = [];
+  //   productsWrapper = shallow(
+  //     <ProductsWrapper data={data} loading={false} fetchProducts={() => {}} />
+  //   );
+  //   // it('should pass the correct data to renderProducts()', () => {
+  //   //   expect(p)
+  //   // });
+  // });
 });
