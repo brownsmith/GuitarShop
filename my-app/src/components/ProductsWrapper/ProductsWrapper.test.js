1bd9f929997ec1f72f54a35e7cca6958f2ea379b
Paul X. Brownsmith <paul.x.brownsmith@waitrose.co.uk>
2020-04-08 14:36:06 +0100

    adding test for loading state

--- my-app/src/components/ProductsWrapper/ProductsWrapper.test.js
+++ my-app/src/components/ProductsWrapper/ProductsWrapper.test.js
@@ -7,8 +7,8 @@ configure({ adapter: new Adapter() });
 
 describe('Product details page', () => {
   let productsWrapper;
+  const data = [];
   it('should render the ProductsWrapper component', () => {
-    const data = [];
     jest.spyOn(React, 'useEffect').mockImplementation(f => f());
     productsWrapper = shallow(
       <ProductsWrapper data={data} loading={false} fetchProducts={() => {}} />
@@ -16,6 +16,13 @@ describe('Product details page', () => {
     expect(productsWrapper.find('.productsWrapperComponent').exists()).toEqual(
       true
     );
+  });
+
+  it('shows the loading spinner if loading is true', () => {
+    productsWrapper = shallow(
+      <ProductsWrapper data={data} loading={false} fetchProducts={() => {}} />
+    );
+    expect(productsWrapper.find('.loader').exists()).toEqual(true);
   });
 
   // it('should dp something else witht productData from the hook', () => {
